d44f449482d582b77bbb763767227fc56b4646b7
dhavaljoshi008 <dhavaljoshi008@gmail.com>
2016-04-21 16:48:39 -0500

    Added Instructor class

---
+++ ECampusConnect-ejb/src/main/java/se/project/ecampusconnect/professor/Instructor.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.project.ecampusconnect.professor;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+/**
+ *
+ * @author Dhaval
+ */
+@Entity
+@Table(name = "Instructor")
+public class Instructor implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "Name")
+    @NotNull(message = "Name cannot be null.")
+    private String name;
+    
+    @Column(name = "Department")
+    @NotNull(message = "Department cannot be null.")
+    private String department;
+
+    /**
+     * Get the value of department
+     *
+     * @return the value of department
+     */
+    public String getDepartment() {
+        return department;
+    }
+
+    /**
+     * Set the value of department
+     *
+     * @param department new value of department
+     */
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+
+    /**
+     * Get the value of name
+     *
+     * @return the value of name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @param name new value of name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Instructor)) {
+            return false;
+        }
+        Instructor other = (Instructor) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "se.project.ecampusconnect.professor.Professor[ id=" + id + " ]";
+    }
+    
+}

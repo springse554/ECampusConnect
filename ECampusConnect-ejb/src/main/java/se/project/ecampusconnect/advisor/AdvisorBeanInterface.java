5ddedfb82c063c6222d9d8b7b6cd68ae00b9dc7b
Advait Patel <advaitpa93@gmail.com>
2016-05-19 10:40:49 -0500

    Added session bean for Advisor

---
+++ ECampusConnect-ejb/src/main/java/se/project/ecampusconnect/advisor/AdvisorBeanInterface.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.project.ecampusconnect.advisor;
+
+/**
+ *
+ * @author Advait
+ */
+public interface AdvisorBeanInterface 
+{
+    public void addAdvisor(String firstName, String lastName, String gender, Integer age);
+   
+}
--- ECampusConnect-ejb/src/main/java/se/project/ecampusconnect/people/Advisor.java
+++ ECampusConnect-ejb/src/main/java/se/project/ecampusconnect/people/Advisor.java
@@ -6,79 +6,80 @@
 package se.project.ecampusconnect.people;
 
 import java.io.Serializable;
-import javax.persistence.Entity;
+import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.NamedQuery;
+import javax.persistence.Version;
 
 /**
  *
  * @author Advait
  */
 @Entity
-@DiscriminatorValue( value = "advisor" )
-public class Advisor extends Staff{
+@NamedQuery(name = "findAllAdvisors", query = "select a from Advisor a")
+public class Advisor implements Serializable
+{
 
+    private static final long serialVersionUID = 1L;
     @Id
+    @Column(name="ADVISOR_ID")
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
-    private String services;
-    
-    public Advisor(int staff_id, String staff_name, String services)
-    {
-        super(staff_id, staff_name);
-        this.services = services;
-    }
-    
-    public Advisor()
-    {
-        super();
-    }
-    
+    private Long customerId;
+    @Version
+    @Column
+    private String firstName;
+    @Column
+    private String lastName;
+    @Column
+    private String gender;
+    @Column
+    private Integer age;
     
     
     public Long getId() {
-        return id;
+        return customerId;
+
     }
 
     public void setId(Long id) {
-        this.id = id;
+        this.customerId = id;
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
+    public String getFirstName() {
+
+        return firstName;
     }
 
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Advisor)) {
-            return false;
-        }
-        Advisor other = (Advisor) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    @Override
-    public String toString() {
-        return "se.project.ecampusconnect.people.Advisor[ id=" + id + " ]";
+    public String getLastName() {
+        return lastName;
     }
 
-    public String getServices() {
-        return services;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
-    public void setServices(String services) {
-        this.services = services;
+    public String getGender() {
+        return gender;
     }
-    
+
+    public void setGender( 
+        String genderText) {
+        this.gender = genderText;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }

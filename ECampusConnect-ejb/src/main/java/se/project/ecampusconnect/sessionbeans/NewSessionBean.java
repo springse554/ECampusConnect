45212b8ebce2efd24c252e7b3650c753a286e83f
Advait Patel <advaitpa93@gmail.com>
2016-06-09 03:32:46 -0500

    Created Session beans for Student and Professor

--- ECampusConnect-ejb/src/main/java/se/project/ecampusconnect/sessionbeans/NewSessionBean.java
+++ ECampusConnect-ejb/src/main/java/se/project/ecampusconnect/sessionbeans/NewSessionBean.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package se.project;
+package se.project.ecampusconnect.sessionbeans;
 
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;

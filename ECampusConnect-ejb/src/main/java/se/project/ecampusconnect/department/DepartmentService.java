30649b3c0f681f3f6b0952d965a66ccb8a17ae37
Advait Patel <advaitpa93@gmail.com>
2016-06-09 16:11:01 -0500

    Created SOAP with Session beans
    Go to Services pkg.

---
+++ ECampusConnect-ejb/src/main/java/se/project/ecampusconnect/department/DepartmentService.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.project.ecampusconnect.department;
+
+/**
+ *
+ * @author Advait
+ */
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+
+@Stateless
+public class DepartmentService implements DepartmentServiceLocal, DepartmentServiceRemote {
+
+    @EJB
+    Department department;
+
+    public DepartmentService() {
+
+    }
+
+    @Override
+    public String service(String computer_science) 
+    {
+        
+        String information_systems = department.getServices().get(computer_science);
+        
+        if(information_systems != null)
+        {
+            return information_systems;
+        }
+        else
+        {
+            return "not found.";
+        }
+        
+    }
+
+}
